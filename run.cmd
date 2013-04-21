3773983ac890d8c41dfed2c5d780704db74b7bb6
Rino <mavladi@gmail.com>
2013-04-22 00:29:37 +0300

    Опция выбора файла xsl

--- run.cmd
+++ run.cmd
@@ -1,6 +1,8 @@
 @echo Start @ %date% %time%
 
-@ java -jar d:\saxon\saxon9pe.jar -a -config:d:\saxon\config.xml %1 -o:%2
+@REM java -jar d:\saxon\saxon9pe.jar -a -config:d:\saxon\config.xml %1 -o:%2
+
+java -jar d:\saxon\saxon9pe.jar -config:d:\saxon\config.xml -xsl:%1 %2 -o:%3 
 
 @echo End @%date% %time%
 @ pause
